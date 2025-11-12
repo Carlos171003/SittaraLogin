5ab25f816ed47837abbae728a614756a0fa41029
Carlos171003 <prepa6acy1d.canul.carlos@gmail.com>
2025-11-12 12:05:46 -0600

    refactor: Refactor RestaurantCard widgets in home_screen.dart

--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -86,8 +86,8 @@ class HomeScreen extends StatelessWidget {
 
                   SizedBox(height: 80), // Add SizedBox here
 
-                  // Tarjeta 2 (RestaurantCard2)
-                  RestaurantCard2(
+                  // Tarjeta 2 (RestaurantCard)
+                  RestaurantCard(
                     imagePath:
                         'assets/images/Apoala.png', // Using a different image for variety
                     name: 'Apoala',
@@ -98,8 +98,8 @@ class HomeScreen extends StatelessWidget {
                       height:
                           80), // Space between RestaurantCard2 and RestaurantCard3
 
-                  // Tarjeta 3 (RestaurantCard3)
-                  RestaurantCard3(
+                  // Tarjeta 3 (RestaurantCard)
+                  RestaurantCard(
                     imagePath:
                         'assets/images/Babes.png', // Using a different image for variety
                     name: 'Babe´s',
@@ -108,8 +108,8 @@ class HomeScreen extends StatelessWidget {
 
                   SizedBox(height: 80), // Space after RestaurantCard3
 
-                  // Tarjeta 4 (RestaurantCard4)
-                  RestaurantCard4(
+                  // Tarjeta 4 (RestaurantCard)
+                  RestaurantCard(
                     imagePath:
                         'assets/images/Bistrola 57.png', // Using a different image for variety
                     name: 'Bistrola 57',
@@ -118,8 +118,8 @@ class HomeScreen extends StatelessWidget {
 
                   SizedBox(height: 80), // Space after RestaurantCard4
 
-                  // Tarjeta 5 (RestaurantCard5)
-                  RestaurantCard5(
+                  // Tarjeta 5 (RestaurantCard)
+                  RestaurantCard(
                     imagePath:
                         'assets/images/Coyote Maya.png', // Using a different image for variety
                     name: 'Coyote Maya',
@@ -128,8 +128,8 @@ class HomeScreen extends StatelessWidget {
 
                   SizedBox(height: 80), // Space after RestaurantCard5
 
-                  // Tarjeta 6 (RestaurantCard6)
-                  RestaurantCard6(
+                  // Tarjeta 6 (RestaurantCard)
+                  RestaurantCard(
                     imagePath:
                         'assets/images/Pita Mediterranea.png', // Using a different image for variety
                     name: 'Pita Mediterranea',
@@ -222,416 +222,6 @@ class RestaurantCard extends StatelessWidget {
             },
           ),
         ),
-      ),
-    );
-  }
-}
-
-class RestaurantCard2 extends StatelessWidget {
-  final String imagePath;
-  final String name;
-  final double rating;
-
-  const RestaurantCard2({
-    super.key,
-    required this.imagePath,
-    required this.name,
-    required this.rating,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(2, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    imagePath,
-                    width: double.infinity, // Fill available width
-                    fit: BoxFit.fitWidth, // Scale proportionally to fit width
-                  ),
-                  Container(
-                    color: Colors.black.withAlpha(102),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starValue = index + 1;
-                            if (starValue <= rating) {
-                              return const Icon(Icons.star,
-                                  color: Colors.amber, size: 18);
-                            } else if (starValue - rating < 1) {
-                              return const Icon(Icons.star_half,
-                                  color: Colors.amber, size: 18);
-                            } else {
-                              return const Icon(Icons.star_border,
-                                  color: Colors.amber, size: 18);
-                            }
-                          }),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RestaurantCard3 extends StatelessWidget {
-  final String imagePath;
-  final String name;
-  final double rating;
-
-  const RestaurantCard3({
-    super.key,
-    required this.imagePath,
-    required this.name,
-    required this.rating,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(2, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    imagePath,
-                    width: double.infinity, // Fill available width
-                    fit: BoxFit.fitWidth, // Scale proportionally to fit width
-                  ),
-                  Container(
-                    color: Colors.black.withAlpha(102),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starValue = index + 1;
-                            if (starValue <= rating) {
-                              return const Icon(Icons.star,
-                                  color: Colors.amber, size: 18);
-                            } else if (starValue - rating < 1) {
-                              return const Icon(Icons.star_half,
-                                  color: Colors.amber, size: 18);
-                            } else {
-                              return const Icon(Icons.star_border,
-                                  color: Colors.amber, size: 18);
-                            }
-                          }),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RestaurantCard4 extends StatelessWidget {
-  final String imagePath;
-  final String name;
-  final double rating;
-
-  const RestaurantCard4({
-    super.key,
-    required this.imagePath,
-    required this.name,
-    required this.rating,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(2, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    imagePath,
-                    width: double.infinity, // Fill available width
-                    fit: BoxFit.fitWidth, // Scale proportionally to fit width
-                  ),
-                  Container(
-                    color: Colors.black.withAlpha(102),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starValue = index + 1;
-                            if (starValue <= rating) {
-                              return const Icon(Icons.star,
-                                  color: Colors.amber, size: 18);
-                            } else if (starValue - rating < 1) {
-                              return const Icon(Icons.star_half,
-                                  color: Colors.amber, size: 18);
-                            } else {
-                              return const Icon(Icons.star_border,
-                                  color: Colors.amber, size: 18);
-                            }
-                          }),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RestaurantCard5 extends StatelessWidget {
-  final String imagePath;
-  final String name;
-  final double rating;
-
-  const RestaurantCard5({
-    super.key,
-    required this.imagePath,
-    required this.name,
-    required this.rating,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(2, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    imagePath,
-                    width: double.infinity, // Fill available width
-                    fit: BoxFit.fitWidth, // Scale proportionally to fit width
-                  ),
-                  Container(
-                    color: Colors.black.withAlpha(102),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starValue = index + 1;
-                            if (starValue <= rating) {
-                              return const Icon(Icons.star,
-                                  color: Colors.amber, size: 18);
-                            } else if (starValue - rating < 1) {
-                              return const Icon(Icons.star_half,
-                                  color: Colors.amber, size: 18);
-                            } else {
-                              return const Icon(Icons.star_border,
-                                  color: Colors.amber, size: 18);
-                            }
-                          }),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RestaurantCard6 extends StatelessWidget {
-  final String imagePath;
-  final String name;
-  final double rating;
-
-  const RestaurantCard6({
-    super.key,
-    required this.imagePath,
-    required this.name,
-    required this.rating,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 6,
-              offset: Offset(2, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    imagePath,
-                    width: double.infinity, // Fill available width
-                    fit: BoxFit.fitWidth, // Scale proportionally to fit width
-                  ),
-                  Container(
-                    color: Colors.black.withAlpha(102),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                        Row(
-                          children: List.generate(5, (index) {
-                            double starValue = index + 1;
-                            if (starValue <= rating) {
-                              return const Icon(Icons.star,
-                                  color: Colors.amber, size: 18);
-                            } else if (starValue - rating < 1) {
-                              return const Icon(Icons.star_half,
-                                  color: Colors.amber, size: 18);
-                            } else {
-                              return const Icon(Icons.star_border,
-                                  color: Colors.amber, size: 18);
-                            }
-                          }),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),  
       ),
     );
   }
